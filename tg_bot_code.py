aa589053412c9ce9ae92e5db7d35d4a104c9e67f
FedosOri <fedorsmolin2006>
2021-12-13 12:46:17 +0300

    Add try to tg_bot_code.py

--- tg_bot_code.py
+++ tg_bot_code.py
@@ -1,19 +1,22 @@
 import telebot
 
-bot = telebot.TeleBot("2125121945:AAGv0F0H9M1wjhej6fvE_y_20EsIcn9e9J0")
+try:
+    bot = telebot.TeleBot("2125121945:AAGv0F0H9M1wjhej6fvE_y_20EsIcn9e9J0")
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    a = str(message.chat.id)
-    bot.send_message(message.chat.id, "Привет, я бот Moroz, я не принимаю сообщения, а только отправляю их.\n"
-                                      "\n"
-                                      "ID: " + a)
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        a = str(message.chat.id)
+        bot.send_message(message.chat.id, "Привет, я бот Moroz, я не принимаю сообщения, а только отправляю их.\n"
+                                          "\n"
+                                          "ID: " + a)
 
 
-@bot.message_handler(content_types=["text", "document", "audio"])
-def start(message):
-    bot.send_message(message.chat.id, "Вы конечно можете писать сообщения, оставляя заметки, но я не отвечу.")
+    @bot.message_handler(content_types=["text", "document", "audio"])
+    def start(message):
+        bot.send_message(message.chat.id, "Вы конечно можете писать сообщения, оставляя заметки, но я не отвечу.")
 
 
-bot.polling()
+    bot.polling()
+except():
+    print("Что-то пошло не так")
