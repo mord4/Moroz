db8b79517098b13f27a127d8492f8cac057ea6e7
FedosOri <fedorsmolin2006>
2021-12-15 19:28:57 +0300

    Delete with from db_code.py

--- db_code.py
+++ db_code.py
@@ -2,27 +2,25 @@ import sqlite3
 
 
 def get_data(table, d):
-    with "notifications.db" as f:
-        conn = sqlite3.connect(f)
-        query = f"SELECT * FROM {table} WHERE date <= '{d}'"
-        r = conn.cursor().execute(query).fetchall()
-        conn.close()
-        return r
+    conn = sqlite3.connect("notifications.db")
+    query = f"SELECT * FROM {table} WHERE date <= '{d}'"
+    r = conn.cursor().execute(query).fetchall()
+    conn.close()
+    return r
 
 
 def delete_data(table, res):
-    with "notifications.db" as f:
-        conn = sqlite3.connect(f)
-        if len(res) == 5:
-            query = f"DELETE FROM {table} " \
-                    f"WHERE date='{res[0]}' and time='{res[1]}' " \
-                    f"and message='{res[2]}' and chat_id='{res[3]}' " \
-                    f"and file='{res[4]}'"
-        else:
-            query = f"DELETE FROM {table} " \
-                    f"WHERE date='{res[0]}' and time='{res[1]}' " \
-                    f"and title='{res[2]}' and text='{res[3]}'"
-        print(res)
-        print(query)
-        conn.execute(query)
-        conn.commit()
+    conn = sqlite3.connect("notifications.db")
+    if len(res) == 5:
+        query = f"DELETE FROM {table} " \
+                f"WHERE date='{res[0]}' and time='{res[1]}' " \
+                f"and message='{res[2]}' and chat_id='{res[3]}' " \
+                f"and file='{res[4]}'"
+    else:
+        query = f"DELETE FROM {table} " \
+                f"WHERE date='{res[0]}' and time='{res[1]}' " \
+                f"and title='{res[2]}' and text='{res[3]}'"
+    print(res)
+    print(query)
+    conn.execute(query)
+    conn.commit()
